b350fcb0af132ce0725f7131edad11fc825b03f9
happywebdiva <git@happywebdiva.com>
2014-02-10 15:16:54 -0600

    Delete comments template from pages

--- page.php
+++ page.php
@@ -10,9 +10,6 @@
     while ( have_posts() ) : the_post();
     get_template_part( 'content', 'page' );
     endwhile;
-		if ( comments_open() || '0' != get_comments_number() ) :
-			comments_template();
-		endif;
   ?>
   </div><!--.twelve.columns-->
 	<div class="four columns omega">
