7b73a1160f62d0a61baaa8310c9dd9d7f507e405
happywebdiva <git@happywebdiva.com>
2014-02-22 14:27:31 -0600

    Add custom header for logo capability

--- header.php
+++ header.php
@@ -36,6 +36,7 @@
   <header class="container" role="banner">
   	<div class="sixteen columns">
         <a href="#main" class="visuallyhidden focusable" id="skiptomain">Skip to content</a>
+        <img src="<?php header_image(); ?>" id="headerlogo" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
         <h1><a href="<?php echo home_url( '/' ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
         <h2><?php bloginfo( 'description' ); ?></h2>
         <div id="main-search-box" role="search"><?php  get_search_form() ?></div>
