6fe6f4f9e815ee854c54e20f484afcfc642c58d9
happywebdiva <git@happywebdiva.com>
2014-02-22 13:43:30 -0600

    Even out comment margins

--- comments.php
+++ comments.php
@@ -9,8 +9,8 @@ if ( post_password_required() ) {
 }
 
 if (comments_open()) : ?>
-  <h4><?php comments_number('', 'One Response', '% Responses' ); // First arg is text for 0 responses ?></h4> 
   <div id="commentblock">
+  <h4><?php comments_number('', 'One Response', '% Responses' ); // First arg is text for 0 responses ?></h4> 
     <ol>
       <?php wp_list_comments(array('avatar_size' => '56')); ?>
     </ol>
