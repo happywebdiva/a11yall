02c5277840cc9a6bb35d164e82eb7b068c7088e2
happywebdiva <git@happywebdiva.com>
2014-02-22 18:43:54 -0600

    Fine tuning for content left gutter

--- 404.php
+++ 404.php
@@ -6,6 +6,7 @@
 <?php get_header(); ?>
 <div class="sixteen columns" id="main">
   	<div class="twelve columns alpha" role="main">
+    <div class="hentry">
 		<h1>Page Not Found</h1>
 	  <p>We're sorry.  The page you were looking for could not be found.</p>
     <p>Perhaps using the search form below would help? </p>
@@ -15,6 +16,7 @@
 			document.getElementById('s') && document.getElementById('s').focus();
 		</script>
 		<?php the_widget('WP_Widget_Recent_Posts', array('number' => 5)); ?>
+    </div>
   </div><!--.twelve.columns-->
 
 	<div class="four columns omega">
