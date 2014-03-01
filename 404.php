c747939f814684fa2e27c563f27849b50263a08c
happywebdiva <git@happywebdiva.com>
2014-03-01 08:16:26 -0600

    Improve 404 by comment out recent posts & make search more obvious

--- 404.php
+++ 404.php
@@ -10,12 +10,15 @@
     <div class="hentry">
 	  <p>We're sorry.  The page you were looking for could not be found.</p>
     <p>Perhaps using the search form below would help? </p>
-		<?php get_search_form(); ?>
+		<div id="search-box-404"><?php get_search_form(); ?></div>
 		<script type="text/javascript">
 			// focus on search field after it has loaded
 			document.getElementById('s') && document.getElementById('s').focus();
 		</script>
-		<?php the_widget('WP_Widget_Recent_Posts', array('number' => 5)); ?>
+		<?php
+			// Uncomment next line if you wish to have five most recent blog posts show on 404 results page  
+			// the_widget('WP_Widget_Recent_Posts', array('number' => 5)); 
+		?>
     </div>
   </div><!--.twelve.columns-->
 
