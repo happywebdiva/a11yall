d320b61dbfda79ccbe6c976b388db71e67a70a88
happywebdiva <git@happywebdiva.com>
2014-03-13 17:52:39 -0500

    Adding a template for pages that need to not show a title

---
+++ page-notitle.php
@@ -0,0 +1,38 @@
+<?php
+/* Template Name: Page Without Title
+ * @package a11yall
+ */
+?>
+<?php get_header(); ?>
+<div class="sixteen columns" id="main">
+  <div class="eleven columns alpha" role="main">
+	<?php 
+    while ( have_posts() ) : the_post();
+	?>
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="entry">
+      <?php
+        if (has_post_thumbnail()) {
+          the_post_thumbnail();
+        }
+        the_content();
+        wp_link_pages( array(
+          'before' => '<p class="page-links">Pages: ',
+          'after'  => '</p>',
+        ) );
+      ?>
+    </div><!--.entry-->
+    <?php edit_post_link('Edit This Page', '<p class="button editlink">', '</p>'); ?>
+  </article>
+	<?php
+    endwhile;
+		if ( comments_open() || '0' != get_comments_number() ) :
+			comments_template();
+		endif;
+  ?>
+  </div><!--.eleven.columns-->
+	<div class="five columns omega">
+  <?php get_sidebar('page'); ?>
+  </div><!--.five.columns-->
+</div><!--.sixteen.columns-->
+<?php get_footer(); ?>
