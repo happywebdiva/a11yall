19427eb5e6ac6726ea0a4598b69bc2e76dd8c5af
happywebdiva <git@happywebdiva.com>
2014-08-05 15:02:17 -0500

    Adding no sidebar or title template

--- page-nosidebar-notitle.php
+++ page-nosidebar-notitle.php
@@ -12,9 +12,6 @@
     while ( have_posts() ) : the_post(); 
 	?>
   <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-      <h1 id="page-title"><?php the_title(); ?></h1>
-    </header><!-- .entry-header -->
     <div class="entry clearfix">
       <?php
         the_content();
