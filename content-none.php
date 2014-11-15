a3df811b17e0577760c416a5892ad3bf53bc0bae
happywebdiva <git@happywebdiva.com>
2014-11-15 10:08:57 -0600

    Per review #20513 tweaking internationalization

--- content-none.php
+++ content-none.php
@@ -3,9 +3,7 @@
  * @package a11yall
  */
 ?>
-<?php 
-	_e('<h1>Not Found</h1>','a11yall');
-	_e('<p>We\'re sorry.  The page you were looking for could not be found.</p>','a11yall');
-	_e('<p>Perhaps using the search form below would help? </p>','a11yall');
-	get_search_form(); 
-?>
+<h1><?php _e( 'Not Found', 'a11yall' );?></h1>
+<p><?php _e( 'We\'re sorry.  The page you were looking for could not be found.', 'a11yall' ); ?></p> 
+<p> <?php _e( 'Perhaps using the search form below would help?', 'a11yall' ); ?> </p>
+<?php get_search_form(); ?> 
