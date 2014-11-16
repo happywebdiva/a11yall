1881d96bf2103da29ad0242615b189a65737c5b1
happywebdiva <git@happywebdiva.com>
2014-11-16 09:16:31 -0600

    Improving formatting when no content

--- content-none.php
+++ content-none.php
@@ -4,6 +4,8 @@
  */
 ?>
 <h1><?php _e( 'Not Found', 'a11yall' );?></h1>
+<div class="hentry">
 <p><?php _e( 'We\'re sorry.  The page you were looking for could not be found.', 'a11yall' ); ?></p> 
 <p> <?php _e( 'Perhaps using the search form below would help?', 'a11yall' ); ?> </p>
 <?php get_search_form(); ?> 
+</div>
