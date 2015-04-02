b47795b2651c91de733dd16490e018ab78f5d99c
happywebdiva <git@happywebdiva.com>
2015-04-02 11:13:11 -0500

    Consolidating search forms & tweaking mobile CSS
    Per 22294, consolidating all search forms to use get_search_form(). Mostly this meant changing the header search box and the CSS. Since it involved cross-browser testing, took opportunity to polish mobile CSS for header.

--- header.php
+++ header.php
@@ -29,13 +29,7 @@
 				?>       
         <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
         <p id="tagline"><?php bloginfo( 'description' ); ?></p>
-        <div id="main-search-box" role="search">
-        <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-          <label for="searchbox" class="visuallyhidden"><?php _e('Enter keywords to search:','a11yall'); ?></label>
-          <input type="text" value="<?php the_search_query(); ?>" name="s" id="searchbox" />
-          <input type="submit" id="searchsubmit" value="<?php _e('Search','a11yall'); ?>" />
-        </form>
-        </div>
+        <div id="main-search-box" role="search"><?php get_search_form(); ?></div>
     </div><!--.sixteen.columns-->
   </header>
 </div><!--.section-->
--- searchform.php
+++ searchform.php
@@ -5,6 +5,6 @@
 ?>
 <form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
   <label for="searchbox2" class="visuallyhidden"><?php _e('Enter keywords to search:','a11yall'); ?></label>
-	<input type="text" value="<?php the_search_query(); ?>" name="s" id="searchbox2" />
+	<input type="text" value="<?php the_search_query(); ?>" name="s" id="searchbox" />
 	<input type="submit" id="searchsubmit" value="Search" />
 </form>
